1743205cc2f9864f25d264559e8dae5747bd58b3
gattomorto24 <gattomorto678@gmail.com>
2026-09-26 20:24:29 +0200

    feat: add Free Roam service worker

---
+++ Free-Roam/sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'fantascuola-free-roam-v03';
+const SHELL = ['./', './index.html', './manifest.webmanifest', '../logo.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((key) => key.startsWith('fantascuola-free-roam-') && key !== CACHE).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET' || new URL(event.request.url).origin !== self.location.origin) return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, copy)).catch(() => {});
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match('./index.html')))
+  );
+});
